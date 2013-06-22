35fdf677586bd27d92dcdd0debd63ebc4ba814ea
Kasim Tuman <kasim@runa.com>
2013-06-21 17:51:36 -0700

    a. made it to work with lein 2 b. added sample session round-trip test

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject com.runa/clj-kryo "1.2.0"
   :description "Clojure library for the Kryo serialization API."
-  :plugins [[s3-wagon-private "1.1.2"]
-            [lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.esotericsoftware.kryo/kryo "2.20"]]
-  :repositories {"releases" {:url "s3p://runa-maven/releases/"}
-                 "snapshots" {:url "s3p://runa-maven/snapshots/"}}
-  :java-source-path "src/java"
+                 [com.esotericsoftware.kryo/kryo "2.21"]]
+  :java-source-paths ["src/java"]
   :aot [clj-kryo.core])
