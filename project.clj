3966a236370d282b41923d61ab513db69a98ad8e
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-03 13:01:53 -0800

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.clojars.runa/clj-kryo "1.4.0"
   :description "Clojure library for the Kryo serialization API."
+  :url "https://github.com/runa-labs/clj-kryo"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.esotericsoftware.kryo/kryo "2.21"]]
   :java-source-paths ["src/java"]
